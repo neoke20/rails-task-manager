93377f114e8da4d9a86c495b50a4887f59850ef2
Kevin Konczak <konczakkevin@gmail.com>
2021-08-10 14:18:06 +0900

    completed create and edit

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,7 +2,37 @@ class TasksController < ApplicationController
   def index
     @tasks = Task.all
   end
+
   def show
     @task = Task.find(params[:id])
   end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to task_path(@task)
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    params["task"]["completed"] == "1" ? @task.completed = true : @task.completed = false
+    # raise
+    @task.update(task_params)
+    redirect_to task_path(@task)
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details)
+  end
+
 end
